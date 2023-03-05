fe69e9d491872eefb8f909f58fd2ebc07ae771b1
Dani Saba <60444842+daniellesaba@users.noreply.github.com>
2023-03-05 01:50:16 -0500

    finished feedback, finished half of the symptoms tracking, put in some other stuff?

--- app/src/main/java/com/example/wichacks/SymptomsTracking.kt
+++ app/src/main/java/com/example/wichacks/SymptomsTracking.kt
@@ -2,10 +2,19 @@ package com.example.wichacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class SymptomsTracking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_symptoms_tracking)
+
+        val subButtonClick = findViewById<Button>(R.id.submitButton)
+        subButtonClick.setOnClickListener {
+            Toast.makeText(
+                applicationContext,
+                "Your response has been recorded", Toast.LENGTH_LONG).show()
+        }
     }
 }
