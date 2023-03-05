cb6e3c418435222c3e4d629c650257bcc08b1083
Klaus Curde <kcurde@gmail.com>
2023-03-05 10:28:20 -0500

    added keeping static data on symptoms

--- app/src/main/java/com/example/wichacks/SymptomsTracking.kt
+++ app/src/main/java/com/example/wichacks/SymptomsTracking.kt
@@ -2,7 +2,9 @@ package com.example.wichacks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.Toast
 
 class SymptomsTracking : AppCompatActivity() {
@@ -15,6 +17,89 @@ class SymptomsTracking : AppCompatActivity() {
             Toast.makeText(
                 applicationContext,
                 "Your response has been recorded", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun onCheckboxClicked(view: View) {
+        if (view is CheckBox) {
+            val checked: Boolean = view.isChecked
+
+            when (view.id) {
+                R.id.acne -> {
+                    if (checked) {
+                        SymptomEntry.acne = true;
+                    }
+                }
+                R.id.anxiety -> {
+                    if (checked) {
+                        SymptomEntry.anxiety = true;
+                    }
+                }
+                R.id.bloating -> {
+                    if (checked) {
+                        SymptomEntry.bloating = true;
+                    }
+                }
+                R.id.bodyaches -> {
+                    if (checked) {
+                        SymptomEntry.bodyaches = true;
+                    }
+                }
+                R.id.cramping -> {
+                    if (checked) {
+                        SymptomEntry.cramping = true;
+                    }
+                }
+                R.id.depression -> {
+                    if (checked) {
+                        SymptomEntry.depression = true;
+                    }
+                }
+                R.id.diarrhea -> {
+                    if (checked) {
+                        SymptomEntry.diarrhea = true;
+                    }
+                }
+                R.id.fatigue -> {
+                    if (checked) {
+                        SymptomEntry.fatigue = true;
+                    }
+                }
+                R.id.flashes -> {
+                    if (checked) {
+                        SymptomEntry.flashes = true;
+                    }
+                }
+                R.id.headache -> {
+                    if (checked) {
+                        SymptomEntry.headache = true;
+                    }
+                }
+                R.id.hunger -> {
+                    if (checked) {
+                        SymptomEntry.hunger = true;
+                    }
+                }
+                R.id.irritable -> {
+                    if (checked) {
+                        SymptomEntry.irritable = true;
+                    }
+                }
+                R.id.libido -> {
+                    if (checked) {
+                        SymptomEntry.libido = true;
+                    }
+                }
+                R.id.mood -> {
+                    if (checked) {
+                        SymptomEntry.mood = true;
+                    }
+                }
+                R.id.sleep -> {
+                    if (checked) {
+                        SymptomEntry.sleep = true;
+                    }
+                }
+            }
         }
     }
 }
