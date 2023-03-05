0058ec1e7c1330004d1b0f9207b71c1f3df39fdf
Dani Saba <60444842+daniellesaba@users.noreply.github.com>
2023-03-05 10:01:15 -0500

    fixed some layout issues, added more unversioned files, started on the medicine tracking

--- app/src/main/java/com/example/wichacks/MedicationTracking.kt
+++ app/src/main/java/com/example/wichacks/MedicationTracking.kt
@@ -1,11 +1,21 @@
 package com.example.wichacks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MedicationTracking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medication_tracking)
+
+
+        val medAddition = findViewById<Button>(R.id.medButton)
+        medAddition.setOnClickListener {
+            val intent = Intent(this, MedicationAdding::class.java)
+            startActivity(intent)
+        }
     }
 }
