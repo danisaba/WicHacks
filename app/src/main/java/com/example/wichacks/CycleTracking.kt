62a2914757124fea18ddc36d42a1bb736fe67cb9
Dani Saba <dls8878@rit.edu>
2023-04-03 03:28:10 -0400

    Added back home button for every page for easy navigation around the app. Slay and purr

--- app/src/main/java/com/example/wichacks/CycleTracking.kt
+++ app/src/main/java/com/example/wichacks/CycleTracking.kt
@@ -1,12 +1,20 @@
 package com.example.wichacks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CalendarView
 
 class CycleTracking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cycle_tracking)
+
+        val backHome = findViewById<Button>(R.id.backToScrollFeed)
+        backHome.setOnClickListener {
+            val intent = Intent(this, ScrollingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/wichacks/WaterTracking.kt
+++ app/src/main/java/com/example/wichacks/WaterTracking.kt
@@ -1,11 +1,19 @@
 package com.example.wichacks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WaterTracking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_water_tracking)
+
+        val backHome = findViewById<Button>(R.id.backToScrollWater)
+        backHome.setOnClickListener {
+            val intent = Intent(this, ScrollingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
